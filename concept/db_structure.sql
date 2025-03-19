ee6b664032e9f846556bb3e6341150e2c8fe7099
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-03-19 16:08:31 -0600

    create database structure diagrams and info

---
+++ concept/db_structure.sql
@@ -0,0 +1,200 @@
+CREATE TABLE IF NOT EXISTS "OrganizationType" (
+	"id" serial NOT NULL UNIQUE,
+	"name" bigint NOT NULL UNIQUE,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Organizations" (
+	"id" serial NOT NULL UNIQUE,
+	"type" bigint NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"abbreviation" varchar(255) NOT NULL,
+	"parentOrganization" bigint NOT NULL,
+	"website" varchar(255) NOT NULL,
+	"identifier" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Persons" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	"lastName" varchar(255),
+	"identifier" varchar(255),
+	"affiliation" bigint NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "IvoaEntries" (
+	"id" serial NOT NULL UNIQUE,
+	"type" bigint NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"identifier" varchar(255) NOT NULL,
+	"ivoaEntry" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "IvoaEntryType" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "FunctionCategories" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	"abbreviation" varchar(255) NOT NULL,
+	"backgroundColor" double precision NOT NULL,
+	"textColor" double precision NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Functionalities" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	"abbbreviation" varchar(255) NOT NULL,
+	"category" bigint NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "FileFormats" (
+	"id" serial NOT NULL UNIQUE,
+	"fullName" varchar(255) NOT NULL,
+	"extension" varchar(255) NOT NULL,
+	"affiliation" bigint NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Licenses" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	"identifier" varchar(255) NOT NULL,
+	"fullText" varchar(255) NOT NULL,
+	"scheme" varchar(255) NOT NULL,
+	"schemeUri" varchar(255) NOT NULL,
+	"uri" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Keywords" (
+	"id" serial NOT NULL UNIQUE,
+	"word" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Awards" (
+	"id" serial NOT NULL UNIQUE,
+	"title" varchar(255) NOT NULL,
+	"identifier" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Images" (
+	"id" serial NOT NULL UNIQUE,
+	"uri" varchar(255) NOT NULL,
+	"description" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Phenomenas" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Softwares" (
+	"id" serial NOT NULL UNIQUE,
+	"publicationDate" timestamp with time zone NOT NULL,
+	"authors" bigint NOT NULL,
+	"publisher" bigint NOT NULL,
+	"relatedInstruments" bigint NOT NULL,
+	"relatedObservatories" bigint NOT NULL,
+	"softwareName" varchar(255) NOT NULL,
+	"versionNumber" varchar(255) NOT NULL,
+	"versionDate" timestamp with time zone NOT NULL,
+	"versionDescription" varchar(255) NOT NULL,
+	"versionPid" varchar(255) NOT NULL,
+	"PersistentIdentifier" varchar(255) NOT NULL,
+	"referencePublication" varchar(255) NOT NULL,
+	"description" varchar(255) NOT NULL,
+	"conciseDescription" varchar(255) NOT NULL,
+	"softwareFunctionality" bigint NOT NULL,
+	"documentation" varchar(255) NOT NULL,
+	"dataInputs" bigint NOT NULL,
+	"supportedFileFormats" bigint NOT NULL,
+	"relatedPublications" varchar(255) NOT NULL,
+	"relatedDatasets" varchar(255) NOT NULL,
+	"developmentStatus" bigint NOT NULL,
+	"operatingSystem" bigint NOT NULL,
+	"metadataLicense" bigint NOT NULL,
+	"license" bigint NOT NULL,
+	"relatedRegion" varchar(255) NOT NULL,
+	"keywords" bigint NOT NULL,
+	"relatedSoftware" varchar(255) NOT NULL,
+	"interopableSoftware" varchar(255) NOT NULL,
+	"funder" bigint NOT NULL,
+	"award" bigint NOT NULL,
+	"codeRepositoryUrl" varchar(255) NOT NULL,
+	"logo" bigint NOT NULL,
+	"relatedPhenomena" bigint NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "RepoStatuses" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "OperatingSystems" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+
+ALTER TABLE "Organizations" ADD CONSTRAINT "Organizations_fk1" FOREIGN KEY ("type") REFERENCES "OrganizationType"("id");
+
+ALTER TABLE "Organizations" ADD CONSTRAINT "Organizations_fk4" FOREIGN KEY ("parentOrganization") REFERENCES "Organizations"("id");
+ALTER TABLE "Persons" ADD CONSTRAINT "Persons_fk4" FOREIGN KEY ("affiliation") REFERENCES "Organizations"("id");
+ALTER TABLE "IvoaEntries" ADD CONSTRAINT "IvoaEntries_fk1" FOREIGN KEY ("type") REFERENCES "IvoaEntryType"("id");
+
+
+ALTER TABLE "Functionalities" ADD CONSTRAINT "Functionalities_fk3" FOREIGN KEY ("category") REFERENCES "FunctionCategories"("id");
+ALTER TABLE "FileFormats" ADD CONSTRAINT "FileFormats_fk3" FOREIGN KEY ("affiliation") REFERENCES "Organizations"("id");
+
+
+
+
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk2" FOREIGN KEY ("authors") REFERENCES "Persons"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk3" FOREIGN KEY ("publisher") REFERENCES "Organizations"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk4" FOREIGN KEY ("relatedInstruments") REFERENCES "IvoaEntries"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk5" FOREIGN KEY ("relatedObservatories") REFERENCES "IvoaEntries"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk15" FOREIGN KEY ("softwareFunctionality") REFERENCES "Functionalities"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk17" FOREIGN KEY ("dataInputs") REFERENCES "Functionalities"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk18" FOREIGN KEY ("supportedFileFormats") REFERENCES "FileFormats"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk21" FOREIGN KEY ("developmentStatus") REFERENCES "RepoStatuses"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk22" FOREIGN KEY ("operatingSystem") REFERENCES "OperatingSystems"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk23" FOREIGN KEY ("metadataLicense") REFERENCES "Licenses"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk24" FOREIGN KEY ("license") REFERENCES "Licenses"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk26" FOREIGN KEY ("keywords") REFERENCES "Keywords"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk29" FOREIGN KEY ("funder") REFERENCES "Organizations"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk30" FOREIGN KEY ("award") REFERENCES "Awards"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk32" FOREIGN KEY ("logo") REFERENCES "Images"("id");
+
+ALTER TABLE "Softwares" ADD CONSTRAINT "Softwares_fk33" FOREIGN KEY ("relatedPhenomena") REFERENCES "Phenomenas"("id");
+

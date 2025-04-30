df4b704435dafaf2931e2e4223a672c5f6f17356
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-04-30 15:15:03 -0600

    some adjustments to test form

--- django/website/forms/test.py
+++ django/website/forms/test.py
@@ -1,8 +1,28 @@
 from django import forms
 
 class TestForm(forms.Form):
-    test_input = forms.CharField(label="Test Input")
-    test_select = forms.ChoiceField(
-        choices=[('a', 'Option A'), ('b', 'Option B')],
-        label="Test Select"
+    
+    boolean_field = forms.BooleanField(label="BooleanField", required=False)
+    choice_field = forms.ChoiceField(
+        label="ChoiceField",
+        choices=[('', '---'), ('a', 'Option A'), ('b', 'Option B')]
+    )
+    multiple_choice_field = forms.MultipleChoiceField(
+        label="MultipleChoiceField",
+        choices=[('x', 'Choice X'), ('y', 'Choice Y')],
+        widget=forms.CheckboxSelectMultiple
+    )
+    
+    char_field = forms.CharField(label="CharField")
+    integer_field = forms.IntegerField(label="IntegerField")
+    email_field = forms.EmailField(label="EmailField")
+    textarea_field = forms.CharField(
+        label="TextArea",
+        widget=forms.Textarea
+    )
+    
+    date_field = forms.DateField(label="DateField", widget=forms.DateInput(attrs={'type': 'date'}))
+    time_field = forms.TimeField(label="TimeField", widget=forms.TimeInput(attrs={'type': 'time'}))
+    datetime_field = forms.DateTimeField(
+        label="DateTimeField", widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})
     )

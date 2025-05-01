033781883463a13fd2a57d586ed324b0821fbb58
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-05-01 03:52:48 -0600

    remove unnecessary controls

--- django/website/forms/test.py
+++ django/website/forms/test.py
@@ -6,35 +6,35 @@ from ..models import Person
 
 class TestForm(forms.Form):
     
-    boolean_field = forms.BooleanField(label="BooleanField", required=False)
+#    boolean_field = forms.BooleanField(label="BooleanField", required=False)
     choice_field = forms.ChoiceField(
         label="ChoiceField",
         choices=[('', '---'), ('a', 'Option A'), ('b', 'Option B')]
     )
-    multiple_choice_field = forms.MultipleChoiceField(
-        label="MultipleChoiceField",
-        choices=[('x', 'Choice X'), ('y', 'Choice Y')],
-        widget=forms.CheckboxSelectMultiple
-    )
-    
-    char_field = forms.CharField(label="CharField")
-    integer_field = forms.IntegerField(label="IntegerField")
-    email_field = forms.EmailField(label="EmailField")
-    textarea_field = forms.CharField(
-        label="TextArea",
-        widget=forms.Textarea
-    )
-    
-    date_field = forms.DateField(label="DateField", widget=forms.DateInput(attrs={'type': 'date'}))
-    time_field = forms.TimeField(label="TimeField", widget=forms.TimeInput(attrs={'type': 'time'}))
-    datetime_field = forms.DateTimeField(
-        label="DateTimeField", widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})
-    )
-
-    choice_field2 = forms.ChoiceField(
-        label="ChoiceField2",
-        choices=[('', '---'), ('a', 'Option A'), ('b', 'Option B')]
-    )
+#    multiple_choice_field = forms.MultipleChoiceField(
+#        label="MultipleChoiceField",
+#        choices=[('x', 'Choice X'), ('y', 'Choice Y')],
+#        widget=forms.CheckboxSelectMultiple
+#    )
+#    
+#    char_field = forms.CharField(label="CharField")
+#    integer_field = forms.IntegerField(label="IntegerField")
+#    email_field = forms.EmailField(label="EmailField")
+#    textarea_field = forms.CharField(
+#        label="TextArea",
+#        widget=forms.Textarea
+#    )
+#    
+#    date_field = forms.DateField(label="DateField", widget=forms.DateInput(attrs={'type': 'date'}))
+#    time_field = forms.TimeField(label="TimeField", widget=forms.TimeInput(attrs={'type': 'time'}))
+#    datetime_field = forms.DateTimeField(
+#        label="DateTimeField", widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})
+#    )
+#
+#    choice_field2 = forms.ChoiceField(
+#        label="ChoiceField2",
+#        choices=[('', '---'), ('a', 'Option A'), ('b', 'Option B')]
+#    )
 
     combo_box = forms.CharField(label="ComboBox")
 

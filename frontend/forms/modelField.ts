a7a6fece1ed95870c632f5c866d638abc4a7e21a
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-05-24 20:44:27 -0600

    start implementing ModelSubfield functionality

--- frontend/forms/modelField.ts
+++ frontend/forms/modelField.ts
@@ -1,19 +1,32 @@
-import { Widget } from "../loader";
+import { RequirementLevel, Widget } from "../loader";
 
 type WidgetType = new (elem: HTMLElement) => Widget;
 
-type KvContainer = { [key: string]: any };
+type PropertyContainer = {
+
+	/** the title that will be shown above the field, if not specified field name will be used */
+	label?: string,
+
+	/** the small font explanation that will be shown below the label */
+	tooltipExplanation?: string,
+
+	/** the tooltip that appears on hover of the information icon */
+	tooltipBestPractice?: string,
+	
+} & { [key: string]: any };
 
 type SerializedFieldStructure = {
 	typeName: string, 
-	subfields: Subfield[]
+	subfields: SerializedSubfield[]
 };
 
-export type Subfield = {
+export type SerializedSubfield = {
 	name: string,
 	type: string | ModelFieldStructure,
 	multi: boolean,
-} & KvContainer;
+	requirement: RequirementLevel,
+	properties: PropertyContainer,
+};
 
 /**
  * Represents a field type with attributes and a subfield layout that in theory 
@@ -28,8 +41,8 @@ export class ModelFieldStructure {
 
 	/** The type name to use to refer to this field structure layout */
 	public typeName: string = "";
-	public topField: Subfield = null;
-	public subfields: Subfield[] = [];
+	public topField: SerializedSubfield = null;
+	public subfields: SerializedSubfield[] = [];
 
 	private constructor() { }
 
@@ -45,10 +58,17 @@ export class ModelFieldStructure {
 		return this.widgetType as WidgetType;
 	}
 	
-	private static modelMap: Map<string, ModelFieldStructure> = new Map();
+	private static fieldStructureMap: Map<string, ModelFieldStructure> = new Map();
 
-	public static BasicWidget(
-		type: WidgetType, multi: boolean = false, attrs: {[key: string]: any}
+	public static getFieldStructure(fieldType: string): ModelFieldStructure {
+		return this.fieldStructureMap.get(fieldType);
+	}
+
+	public static basicWidget(
+		type: WidgetType, 
+		requirement = RequirementLevel.OPTIONAL, 
+		multi: boolean = false, 
+		attrs: PropertyContainer,
 	) {
 		const model = new ModelFieldStructure();
 		model.widgetType = type;
@@ -56,14 +76,11 @@ export class ModelFieldStructure {
 			name: "SELF",
 			type: model,
 			multi: multi,
-			...attrs,
+			requirement: requirement,
+			properties: attrs,
 		}
 		model.subfields.push(model.topField);
 		return model;
-	}
-
-	public static isSubfieldValidated(subfield: Subfield): boolean {
-		return subfield.topField !== undefined;
 	}
 
 	/**
@@ -82,14 +99,14 @@ export class ModelFieldStructure {
 
 			// top field will always be first field in subfields
 			model.topField = obj.subfields.shift();
-			this.modelMap.set(model.typeName, model);
+			this.fieldStructureMap.set(model.typeName, model);
 		}
 
 		// convert subfield types from strings to references
 		for(const model of models){
 			for(const subfield of model.subfields) {
 				if(typeof subfield.type === "string") {
-					let type = this.modelMap.get(subfield.type);
+					let type = this.fieldStructureMap.get(subfield.type);
 					if (type != null) subfield.type = type;
 					else console.error(
 						`Invalid type '${subfield.type}' on subfield ` + 
@@ -100,5 +117,66 @@ export class ModelFieldStructure {
 		}
 
 		return models;
+	}
+}
+
+/**
+ * represents a single field in the db model and information related to 
+ * rendering/interacting with it in the form
+ */
+export class ModelSubfield {
+
+	public name: string = "";
+
+	public type: ModelFieldStructure = null;
+
+	public multi: boolean = false;
+
+	public requirement: RequirementLevel = RequirementLevel.OPTIONAL;
+
+	public properties: PropertyContainer = {};
+
+	public constructor(
+		name: string, 
+		type: ModelFieldStructure, 
+		multi: boolean = false,
+		requirement: RequirementLevel = RequirementLevel.OPTIONAL,
+		properties: PropertyContainer = {},
+	) {
+		this.name = name;
+		this.type = type;
+		this.multi = multi;
+		this.requirement = requirement;
+		this.properties = properties;
+	}
+
+	/** 
+	 * builds the html ui elements required to render and interact with this 
+	 * field for the db model
+	 * @param targetDiv the target container to build the ui inside of
+	 */
+	public buildWidgets(targetDiv: HTMLDivElement): void {
+		
+	}
+
+	/** parse serialized subfield data into a functional subfield field object */
+	public static parse(data: SerializedSubfield): ModelSubfield {
+
+		// get field structure type if not yet parsed
+		let type = data.type; 
+		if(!(type instanceof ModelFieldStructure)) {
+			type =  ModelFieldStructure.getFieldStructure(type);
+			data.type = type;
+		}
+
+		// create and return the subfield object
+		const field = new ModelSubfield(
+			data.name, 
+			type, 
+			data.multi,
+			data.requirement,
+			data.properties,
+		);
+		return field;
 	}
 }

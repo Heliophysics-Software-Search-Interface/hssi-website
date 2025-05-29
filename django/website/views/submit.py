4194dcda7914a4324f57f92dc3b0e47f15844dba
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-05-29 04:07:59 -0600

    made submission link work

--- django/website/views/submit.py
+++ django/website/views/submit.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 def submit_resource(request):
-    return HttpResponse("TODO")
+    return render(
+        request, 
+        "pages/model_form.html", 
+        { "structure_name": "SubmissionForm" }
+    )

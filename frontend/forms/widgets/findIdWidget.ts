e26d2a653956b9e2d1a6bca01f3d0be5a7faf97b
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-07-29 18:59:08 -0600

    add placeholder text examples

--- frontend/forms/widgets/findIdWidget.ts
+++ frontend/forms/widgets/findIdWidget.ts
@@ -14,6 +14,7 @@ export abstract class FindIdWidget extends UrlWidget {
 	private emphasized: boolean = false;
 	protected findButton: HTMLButtonElement = null;
 	protected get findButtonText(): string { return "find"; }
+	protected get placeholderExample(): string { return ""; }
 
 	protected abstract onFindButtonPressed(): void;
 	public onDataSelected(data: JSONObject): void { }
@@ -53,11 +54,17 @@ export abstract class FindIdWidget extends UrlWidget {
 	override initialize(): void {
 		super.initialize();
 		this.buildFindButton();
-		this.inputElement.placeholder = "Use 'find' button or paste URL";
+		this.inputElement.placeholder = "Use 'find' button or paste URL ";
+		if(this.placeholderExample) {
+			this.inputElement.placeholder += "(ex: " + this.placeholderExample + ")";
+		}
 	}
 }
 
 export class RorWidget extends FindIdWidget {
+	protected get placeholderExample(): string { 
+		return "https://ror.org/00000xxxx";
+	}
 	protected override onFindButtonPressed(): void {
 		const rorPopup = RorFinder.getInstance()
 			.setTarget(this.parentField)
@@ -67,6 +74,9 @@ export class RorWidget extends FindIdWidget {
 }
 
 export class OrcidWidget extends FindIdWidget {
+	protected get placeholderExample(): string { 
+		return "https://orcid.org/0000-0000-0000-0000"; 
+	}
 	protected override onFindButtonPressed(): void {
 		const orcidPopup = OrcidFinder.getInstance()
 			.setTarget(this.parentField)
@@ -106,6 +116,9 @@ export class OrcidWidget extends FindIdWidget {
 }
 
 export class DataciteDoiWidget extends FindIdWidget {
+	protected get placeholderExample(): string { 
+		return "https://doi.org/10.5281/zenodo.00000000";
+	}
 	protected override onFindButtonPressed(): void {
 		const popup = DoiDataciteFinder.getInstance()
 			.setTarget(this.parentField)

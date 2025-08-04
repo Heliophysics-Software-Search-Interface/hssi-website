58f0246cf75dfb328e20225a622ab8cd25d7c958
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-08-04 16:49:51 -0600

    implement adminside retroactive fix for recursive uuid references in some tables

--- django/website/admin/model_admin.py
+++ django/website/admin/model_admin.py
@@ -1,103 +1,159 @@
+import uuid
+
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from django.contrib.admin import action, register
+from django.http import HttpRequest
+from django.db.models import QuerySet, ManyToManyField
 
 from ..models.people import Person, Curator
 from ..models.software import Software, VisibleSoftware, SoftwareVersion
 from ..models.submission_info import SubmissionInfo
 from ..models.auxillary_info import RelatedItem, Award
 from ..models.roots import (
-	FunctionCategory, OperatingSystem, Phenomena, Keyword, Image, 
+	HssiModel,	FunctionCategory, OperatingSystem, Phenomena, Keyword, Image, 
 	Organization, License, InstrumentObservatory, RepoStatus, DataInput,
 	ProgrammingLanguage, FileFormat, Region
 )
+
+from ..util import find_database_references
+
+# Abstract definitions for model admins ----------------------------------------
+
+class HSSIModelAdmin(ImportExportModelAdmin):
+
+	@action(description="Fix Improper Toplevel UUID")
+	def fix_uuid_chains(self, request: HttpRequest, queryset: QuerySet):
+		"""
+		"""
+		hssimodel = queryset.model
+		if not issubclass(hssimodel, HssiModel): return
+		topfield = hssimodel.get_top_field()
+	
+		for obj in queryset:
+			iter_obj = obj
+			val: str = ""
+			uid: uuid.UUID = None
+			iters: int = 0
+			while True:
+				val = getattr(iter_obj, topfield.name)
+				try: uid = uuid.UUID(val)
+				except Exception: break
+				iter_obj = hssimodel.objects.get(pk=uid)
+				iters += 1
+				if iters >= 100: break
+			if iters <= 0: 
+				print(f"'{str(val)}' does not appear to be a uuid")
+				continue
+			print(f"uuid depth for {str(obj)}: {iters}")
+			refobj = hssimodel.objects.filter(pk=uid).first()
+			if not refobj:
+				print(f"could not find {uid} in {hssimodel._meta.model_name}")
+				continue
+			
+			refs = find_database_references(obj)
+			print(f"updating {len(refs)} references to {str(uid)}..")
+			for ref in refs:
+				target, field = ref
+				if isinstance(field, ManyToManyField): 
+					manager = getattr(target, field.name)
+					manager.remove(obj)
+					manager.add(refobj)
+				else: setattr(target, field.name, refobj)
+				target.save()
+				print(f"updated '{target}:{field}' field")
+			# obj.delete()
+	
+	# actions need to be specified
+	actions = [fix_uuid_chains]
 
 # Admin definitions for roots module -------------------------------------------
 
 class OperatingSystemResource(resources.ModelResource):
 	class Meta: model = OperatingSystem
-class OperatingSystemAdmin(ImportExportModelAdmin): resource_class = OperatingSystemResource
+class OperatingSystemAdmin(HSSIModelAdmin): resource_class = OperatingSystemResource
 
 class PhenomenaTypeResource(resources.ModelResource):
 	class Meta: model = Phenomena
-class PhenomenaTypeAdmin(ImportExportModelAdmin): resource_class = PhenomenaTypeResource
+class PhenomenaTypeAdmin(HSSIModelAdmin): resource_class = PhenomenaTypeResource
 
 class KeywordResource(resources.ModelResource):
 	class Meta: model = Keyword
-class KeywordAdmin(ImportExportModelAdmin): resource_class = KeywordResource
+class KeywordAdmin(HSSIModelAdmin): resource_class = KeywordResource
 
 class ImageResource(resources.ModelResource):
 	class Meta: model = Image
-class ImageAdmin(ImportExportModelAdmin): resource_class = ImageResource
+class ImageAdmin(HSSIModelAdmin): resource_class = ImageResource
 
 class RegionResource(resources.ModelResource):
 	class Meta: model = Region
-class RegionAdmin(ImportExportModelAdmin): resource_class = RegionResource
+class RegionAdmin(HSSIModelAdmin): resource_class = RegionResource
 
 class DataInputResource(resources.ModelResource):
 	class Meta: model = DataInput
-class DataInputAdmin(ImportExportModelAdmin): resource_class = DataInputResource
+class DataInputAdmin(HSSIModelAdmin): resource_class = DataInputResource
 
 class RepoStatusResource(resources.ModelResource):
 	class Meta: model = RepoStatus
-class RepoStatusAdmin(ImportExportModelAdmin): resource_class = RepoStatusResource
+class RepoStatusAdmin(HSSIModelAdmin): resource_class = RepoStatusResource
 
 class FunctionCategoryResource(resources.ModelResource):
 	class Meta: model = FunctionCategory
-class FunctionCategoryAdmin(ImportExportModelAdmin): resource_class = FunctionCategoryResource
+class FunctionCategoryAdmin(HSSIModelAdmin): resource_class = FunctionCategoryResource
 
 class InstrumentObservatoryResource(resources.ModelResource):
 	class Meta: model = InstrumentObservatory
-class InstrumentObservatoryAdmin(ImportExportModelAdmin): resource_class = InstrumentObservatoryResource
+class InstrumentObservatoryAdmin(HSSIModelAdmin): resource_class = InstrumentObservatoryResource
 
 class LicenseResource(resources.ModelResource):
 	class Meta: model = License
-class LicenseAdmin(ImportExportModelAdmin): resource_class = LicenseResource
+class LicenseAdmin(HSSIModelAdmin): resource_class = LicenseResource
 
 class OrganizationResource(resources.ModelResource):
 	class Meta: model = Organization
-class OrganizationAdmin(ImportExportModelAdmin): resource_class = OrganizationResource
+class OrganizationAdmin(HSSIModelAdmin): resource_class = OrganizationResource
 
 class FileFormatResource(resources.ModelResource):
 	class Meta: model = FileFormat
-class FileFormatAdmin(ImportExportModelAdmin): resource_class = FileFormatResource
+class FileFormatAdmin(HSSIModelAdmin): resource_class = FileFormatResource
 
 class ProgrammingLanguageResource(resources.ModelResource):
 	class Meta: model = ProgrammingLanguage
-class ProgrammingLanguageAdmin(ImportExportModelAdmin): resource_class = ProgrammingLanguageResource
+class ProgrammingLanguageAdmin(HSSIModelAdmin): resource_class = ProgrammingLanguageResource
 
 # Admin definitions for people module ------------------------------------------
 
 class PersonResource(resources.ModelResource): 
 	class Meta: model = Person
-class PersonAdmin(ImportExportModelAdmin): resource_class = PersonResource
+class PersonAdmin(HSSIModelAdmin): resource_class = PersonResource
 
 class CuratorResource(resources.ModelResource):
 	class Meta: model = Curator
-class CuratorAdmin(ImportExportModelAdmin): resource_class = CuratorResource
+class CuratorAdmin(HSSIModelAdmin): resource_class = CuratorResource
 
 class SubmitterResource(resources.ModelResource):
 	class Meta: model = Person
-class SubmitterAdmin(ImportExportModelAdmin): resource_class = SubmitterResource
+class SubmitterAdmin(HSSIModelAdmin): resource_class = SubmitterResource
 
 # Admin definitions for Auxillary Info -----------------------------------------
 
 class AwardResource(resources.ModelResource):
 	class Meta: model = Award
-class AwardAdmin(ImportExportModelAdmin): resource_class = AwardResource
+class AwardAdmin(HSSIModelAdmin): resource_class = AwardResource
 
 class DatasetResource(resources.ModelResource):
 	class Meta: model = RelatedItem
-class DatasetAdmin(ImportExportModelAdmin): resource_class = DatasetResource
+class DatasetAdmin(HSSIModelAdmin): resource_class = DatasetResource
 
 class SoftwareVersionResource(resources.ModelResource):
 	class Meta: model = SoftwareVersion
-class SoftwareVersionAdmin(ImportExportModelAdmin): resource_class = SoftwareVersionResource
+class SoftwareVersionAdmin(HSSIModelAdmin): resource_class = SoftwareVersionResource
 
 # Admin definitions for software module ----------------------------------------
 
 class SoftwareResource(resources.ModelResource):
 	class Meta: model = Software
-class SoftwareAdmin(ImportExportModelAdmin): resource_class = SoftwareResource
+class SoftwareAdmin(HSSIModelAdmin): resource_class = SoftwareResource
 
 class VisibleSoftwareResource(resources.ModelResource):
 	class Meta: model = VisibleSoftware
@@ -107,4 +163,4 @@ class VisibleSoftwareAdmin(ImportExportModelAdmin): resource_class = VisibleSoft
 
 class SubmissionInfoResource(resources.ModelResource):
 	class Meta: model = SubmissionInfo
-class SubmissionInfoAdmin(ImportExportModelAdmin): resource_class = SubmissionInfoResource
+class SubmissionInfoAdmin(HSSIModelAdmin): resource_class = SubmissionInfoResource

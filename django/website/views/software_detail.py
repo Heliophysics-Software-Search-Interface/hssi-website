50f65dc4a794975f36b66f06975ce6085a89d0f1
Isaiah Smith <isaiahnsmith@hotmail.com>
2026-03-14 15:37:05 -0600

    fix software detail template to work with updated software model

--- django/website/views/software_detail.py
+++ django/website/views/software_detail.py
@@ -16,3 +16,16 @@ class SoftwareDetailView(generic.DetailView):
         # Only show visible (published) software
         visible_ids = VerifiedSoftware.objects.values_list('id', flat=True)
         return Software.objects.filter(pk__in=visible_ids)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        software = context.get('software')
+        latest_version = None
+        if software is not None:
+            latest_version = (
+                software.version.all()
+                .order_by('-release_date', '-number')
+                .first()
+            )
+        context['latest_version'] = latest_version
+        return context
